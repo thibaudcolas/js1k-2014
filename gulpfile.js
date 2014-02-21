b6b58352ea33543345bda6923ff9af722a347a69
Thibaud Colas <thibaudcolas@gmail.com>
2014-02-21 21:07:27 +0100

    Cleanup before work starts

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,55 @@
 var gulp = require('gulp');
-var js1k = require("gulp-js1k");
+var path = require('path');
+var open = require('open');
+var express = require ('express');
+var lr = require('tiny-lr');
+var js1k = require('gulp-js1k');
+
+var server = lr();
+var app = express();
+
+var APP_ROOT = __dirname + '/source';
+var APP_PORT = 4000;
+
+// Opens a browser with the application path.
+gulp.task('open', ['serve'], function(){
+  open('http://localhost:' + APP_PORT);
+});
+
+// Statically serves files and adds the LiveReload script.
+gulp.task('serve', function () {
+  app.use(require('connect-livereload')());
+  app.use(express.static(APP_ROOT));
+  app.listen(APP_PORT);
+});
+
+// Watches for file changes and reloads browser pages.
+gulp.task('watch', function () {
+  server.listen(35729, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+
+    gulp.watch([
+      'source/**/*'
+    ], function (evt) {
+      server.changed({
+        body: {
+          files: [path.relative(APP_ROOT, evt.path)]
+        }
+      });
+    });
+  });
+});
 
 // Print some stats (js1k elgibility), minify and output to "/submission/"
-gulp.task("js1k", function() {
-    gulp.src("original/*.js")
-        .pipe(js1k())
-        .pipe(gulp.dest("submission/"));
+gulp.task('build', function() {
+  gulp.src('original/*.js')
+      .pipe(js1k())
+      .pipe(gulp.dest('build/'));
 });
 
-// Print some stats (js1k elgibility), minify, output to "/submission/" with the shim html file
-gulp.task("js1k-shim", function() {
-    gulp.src("original/*.js")
-        .pipe(js1k(true)) // Notice the true
-        .pipe(gulp.dest("submission/"));
-});
+// Default developer working task.
+gulp.task('work', ['watch', 'open']);
+
+gulp.task('default', ['work']);
